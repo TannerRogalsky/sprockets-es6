370af587f0554448e9565d3134719f4c2d5baebd
Joshua Peek <josh@joshpeek.com>
2015-02-07 22:11:45 -0800

    Test module output

--- test/test_es6.rb
+++ test/test_es6.rb
@@ -19,4 +19,73 @@ var square = function (n) {
 };
     JS
   end
+
+  def test_common_modules
+    register Sprockets::ES6.new('modules' => 'common')
+    assert asset = @env["mod.js"]
+    assert_equal 'application/javascript', asset.content_type
+    assert_equal <<-JS.chomp, asset.to_s.strip
+"use strict";
+
+require("foo");
+    JS
+  end
+
+  def test_amd_modules
+    register Sprockets::ES6.new('modules' => 'amd')
+    assert asset = @env["mod.js"]
+    assert_equal 'application/javascript', asset.content_type
+    assert_equal <<-JS.chomp, asset.to_s.strip
+define(["exports", "foo"], function (exports, _foo) {
+  "use strict";
+});
+    JS
+  end
+
+  def test_amd_modules_with_ids
+    register Sprockets::ES6.new('modules' => 'amd', 'moduleIds' => true)
+    assert asset = @env["mod.js"]
+    assert_equal 'application/javascript', asset.content_type
+    assert_equal <<-JS.chomp, asset.to_s.strip
+define("unknown", ["exports", "foo"], function (exports, _foo) {
+  "use strict";
+});
+    JS
+  end
+
+  def test_system_modules
+    register Sprockets::ES6.new('modules' => 'system')
+    assert asset = @env["mod.js"]
+    assert_equal 'application/javascript', asset.content_type
+    assert_equal <<-JS.chomp, asset.to_s.strip
+System.register(["foo"], function (_export) {
+  "use strict";
+
+  return {
+    setters: [function (_foo) {}],
+    execute: function () {}
+  };
+});
+    JS
+  end
+
+  def test_system_modules_with_ids
+    register Sprockets::ES6.new('modules' => 'system', 'moduleIds' => true)
+    assert asset = @env["mod.js"]
+    assert_equal 'application/javascript', asset.content_type
+    assert_equal <<-JS.chomp, asset.to_s.strip
+System.register("unknown", ["foo"], function (_export) {
+  "use strict";
+
+  return {
+    setters: [function (_foo) {}],
+    execute: function () {}
+  };
+});
+    JS
+  end
+
+  def register(processor)
+    @env.register_transformer 'text/ecmascript-6', 'application/javascript', processor
+  end
 end
